58d23002259e5d37a7b75102ef5bca11bf37ca49
aissa-m <issamaymoutt17@gmail.com>
2025-11-03 01:05:23 +0100

    solving orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { PrismaModule } from '../prisma/prisma.module';
+import { CommonModule } from '../common/common.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
+  imports: [PrismaModule, CommonModule, NotificationsModule],
   controllers: [OrdersController],
   providers: [OrdersService],
   exports: [OrdersService],
